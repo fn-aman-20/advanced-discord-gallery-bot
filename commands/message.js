13a889adc8627a9b27114023a8ee4c2c479c040c
Aman <99836388+fn-aman-20@users.noreply.github.com>
2022-09-14 15:56:01 +0530

    message settings command

---
+++ commands/message.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = new SlashCommandBuilder()
+.setName('message')
+.setDescription('configure or view message settings')
+.addBooleanOption(bot => bot
+  .setName('autodelete')
+  .setDescription('auto delete other bot messages?'))
+.addRoleOption(role => role
+  .setName('role')
+  .setDescription('members with this role can bypass filters (for mods)'))
+.addUserOption(user => user
+  .setName('user')
+  .setDescription('this user can bypass filters (for friends)'))
