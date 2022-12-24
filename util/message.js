9461e44ed7d60889678e1b59dd12058d17185dda
Aman <99836388+fn-aman-20@users.noreply.github.com>
2022-12-24 17:32:11 +0530

    Update message.js

--- util/message.js
+++ util/message.js
@@ -2,16 +2,16 @@ const { EmbedBuilder } = require('discord.js');
 
 let noEmbeds = new EmbedBuilder()
   .setColor('Random')
-  .setTitle(`<:cross_mark:988096131714121749> No embeds were found in your message! React or create thread(s) to start a conversation instead like the one below`)
-  .setImage('https://cdn.discordapp.com/attachments/926364390062686238/1010073179051532308/Embedonly_2.jpg')
+  .setTitle(`❌ No embeds were found in your message! React or create thread(s) to start a conversation instead like the one below`)
+  .setImage('https://cdn.discordapp.com/attachments/1055441982115295245/1056132727549739038/Screenshot_20221224_022347.png')
   .setFooter({
     text: 'Embeds are links that show a preview of their content when sent in a channel'
   });
 
 let noAttachments = new EmbedBuilder()
   .setColor('Random')
-  .setTitle(`<:cross_mark:988096131714121749> No attachments were found in your message! React or create thread(s) to start a conversation instead like the one below`)
-  .setImage('https://cdn.discordapp.com/attachments/926364390062686238/1010073178753749062/Mediaonly_1.jpg');
+  .setTitle(`❌ No attachments were found in your message! React or create thread(s) to start a conversation instead like the one below`)
+  .setImage('https://cdn.discordapp.com/attachments/1055441982115295245/1056132727159652372/Screenshot_20221224_022257.png');
 
 module.exports = {
   noEmbeds: noEmbeds,
