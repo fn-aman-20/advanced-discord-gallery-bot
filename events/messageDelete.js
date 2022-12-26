192873735ba0d2d9303d01127cfbb7ffcc222c56
Aman <99836388+fn-aman-20@users.noreply.github.com>
2022-12-26 19:40:35 +0530

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,5 +1,5 @@
 module.exports = async (client, message) => {
-  if (!message.hasThread || message.author.bot) return;
+  if (!message.hasThread) return;
   const data = client.db.get(message.guild.id),
   list = Object.keys(data.embedonly).concat(Object.values(data.mediaonly));
   if (list.includes(message.channel.id)) return await message.thread.delete();
